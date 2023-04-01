b05b8d2067720c80a137a7067faca8b3244d43ce
Maggie <96947247+MaggieLOL@users.noreply.github.com>
2023-04-01 17:22:56 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
 imgHostWhitelist=["i.imgur.com", "imgur.com", "share.lyka.pro", "cdn.discordapp.com", "i.gyazo.com", "img.thz.cool", "i.loli.net", "s2.loli.net", "s1.ax1x.com", "s2.ax1x.com", "z3.ax1x.com", "s4.ax1x.com", "i.postimg.cc", "mrpig.eu.org", "gimg2.baidu.com", "files.catbox.moe", "img.liyuv.top"]
-console.log("Dynamic Image whitelists loaded.")
+console.log("Dynamic Image whitelists loaded. Last update: Apr 1")
